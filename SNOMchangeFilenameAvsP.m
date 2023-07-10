5604fed129f1a06dea8cb103a6eccf31e855ecdf
Németh Gergely <nemeth.gergely@protonmail.com>
2023-07-10 23:46:29 +0200

    Automatic amplitude file search for phase rotation

---
+++ SNOMchangeFilenameAvsP.m
@@ -0,0 +1,47 @@
+function [newfilename] = SNOMchangeFilenameAvsP(fileName,meastype)
+% change the file name to read a different harmonics
+% dataTypeBoolean is 1 if type is Amplitude and 0 if type if Phase or Z
+
+% indx = regexp(fileName,'.gsf');
+% input parsing ----------------------------------
+p = inputParser;
+validfileName = @(x) ischar(x);
+validmeasType = @(x) ischar(x) && (length(x) == 1) && ((x == 'P') || (x == 'A'));
+addRequired(p,'fileName',validfileName);
+addRequired(p,'meastype',validmeasType);
+
+
+parse(p,fileName,meastype)
+%-------------------------------------------------
+
+%for dump file - OLD snom
+if isempty(regexp(fileName,'.gsf')) == 1
+    indx = regexp(fileName,'.dump');
+    Type = fileName(indx-8:indx-1);
+end
+
+%for gsf file - NEW snom
+if ~isempty(regexp(fileName,'.gsf')) == 1
+    indx = regexp(fileName,'.gsf');
+    if strcmp(fileName(indx-3:indx-1),'raw') == 1
+        Type = fileName(indx-8:indx-4);
+        if strcmp(Type(1),'-') == 1
+            Type = fileName(indx-10:indx-4);
+        end
+        if ~isempty(regexp(Type,' Z')) == 1
+            Type = fileName(indx-6:indx-1);
+        elseif ~isempty(regexp(Type,'-Z')) == 1
+            Type = fileName(indx-8:indx-1);
+        end
+    else
+        Type = fileName(indx-4:indx-1);
+        if strcmp(Type(1),'-') == 1
+            Type = fileName(indx-5:indx-1);
+        else
+        end
+    end
+    
+    idx = regexp(fileName,Type);
+    newfilename = fileName;
+    newfilename(idx+3) = meastype;
+end
